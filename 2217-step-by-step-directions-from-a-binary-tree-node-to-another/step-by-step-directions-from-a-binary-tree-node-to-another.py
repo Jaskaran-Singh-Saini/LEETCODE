949c7809c35ff2ab610e8d1cfce1cba1a82d4a6f
Jaskaran Singh <jaskaransingh0924@gmail.com>
2026-08-08 13:55:05 +0530

    Time: 78 ms (53.06%) | Memory: 73.9 MB (47.05%) - LeetSync

---
+++ 2217-step-by-step-directions-from-a-binary-tree-node-to-another/step-by-step-directions-from-a-binary-tree-node-to-another.py
@@ -0,0 +1,36 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def getDirections(self, root: Optional[TreeNode], startValue: int, destValue: int) -> str:
+        def dfs(node, path, target):
+            if  not node:
+                return ""
+
+            if node.val == target:
+                return path
+            
+            path.append("L")
+            res = dfs(node.left, path, target)
+            if res: return res
+            path.pop()
+
+            path.append("R")
+            res = dfs(node.right, path, target)
+            if res: return res
+            path.pop()
+            return ""
+
+        startpath = dfs(root, [], startValue)
+        destpath = dfs(root, [], destValue)
+        i = 0
+        while i < min(len(startpath), len(destpath)):
+            if startpath[i] != destpath[i]:
+                break
+            
+            i += 1
+        res = ["U"] * len(startpath[i:]) + destpath[i:]
+        return "".join(res)
